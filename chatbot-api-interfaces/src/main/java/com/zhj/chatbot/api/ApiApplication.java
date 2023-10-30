5c7ba38616475526edd40563229b075be6b80e0b
Zhj <1342497867@qq.com>
2023-10-30 20:13:58 +0800

    初始化DDD工程

---
+++ chatbot-api-interfaces/src/main/java/com/zhj/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.zhj.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhj
+ * @version 1.0
+ * @date 2023/10/30 19:56
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/com/zhj/chatbot/api/ApiTest.java
@@ -0,0 +1,10 @@
+package com.zhj.chatbot.api;
+
+/**
+ * @author zhj
+ * @version 1.0
+ * @description 单元测试
+ * @date 2023/10/30 20:07
+ */
+public class ApiTest {
+}
